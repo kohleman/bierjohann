66f49d770465d53f521610d82631059a6778bd08
kohleman <manuel.kohler@id.ethz.ch>
2017-06-18 18:43:49 +0200

    Initial Commit

---
+++ bierjohann/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bierjohann
+//
+//  Created by Kohler Manuel on 18.06.17.
+//  Copyright © 2017 Kohler  Manuel (ID SIS). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
